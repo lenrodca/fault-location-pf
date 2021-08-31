5f5f7bc8a4fff3a60f78b8dcc761401768fa1517
lenrodca <campbelll@uninorte.edu.co>
2021-08-30 21:47:23 -0500

    Version preliminar

--- FaultLocalization.py
+++ FaultLocalization.py
@@ -7,7 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import signal
 
-data = pd.read_csv("Prueba_Vol_Cor_V2.1.csv", delimiter=";")     
+data = pd.read_csv("Prueba_Vol_Cor_V2.1.csv", delimiter=";")        
 
 flag = data.loc[data['All calculations'] == '0,000100'].index[0]
 
@@ -29,21 +29,9 @@ current_deltaC = data.iloc[flag:, [0,4]].astype(float).to_numpy()
 voltage_deltaC =data.iloc[ flag:,[0,6]].astype(float).to_numpy()
 
 
-#FILTER
-  # Cut-off frequency of the filter
-w = 0.8
-# Normalize the frequency
-b1, a1 = signal.butter(5, w, 'low')
-outputA = signal.filtfilt(b1, a1, current_deltaA,axis=0)
-
-b2, a2 = signal.butter(5, w, 'low')
-outputB = signal.filtfilt(b2, a2, current_deltaB,axis=0)
-
-b3, a3 = signal.butter(5, w, 'low')
-outputC = signal.filtfilt(b3, a3, current_deltaC,axis=0)
 
 # ADC
-delta_t = 1e-04
+delta_t = 0.0001
 k = 1/ (1.56e06/2)
 theta = 0
 
@@ -51,27 +39,14 @@ theta = 0
 Z = 20.631 + 89.93j
 Y = 676.75e-06j
 
-# ZC = np.sqrt(Z/Y)
+ZC = np.sqrt(Z/Y)
 
 # print(ZC)
 
 #  TRANSFORMACION MODAL
 
-phi = k*delta_t*2*np.pi*60 + theta
-
-T_dq0 = (2/3) * np.array([[0.5, 0.5, 0.5], [np.cos(phi),np.cos(phi-np.pi), np.cos(phi+np.pi) ], [-np.sin(phi),-np.sin(phi-np.pi), -np.sin(phi+np.pi)]])
-
-outputA_fft = np.fft.fft(outputA[:,1])
-outputB_fft = np.fft.fft(outputB[:,1])
-outputC_fft = np.fft.fft(outputC[:,1])
-
 w = 2*np.pi*60
-t = outputA[:,0]
-print(outputA_fft)
-
-outputA_sym = np.abs(outputA_fft)*np.cos(w*t+np.angle(outputA_fft))
-outputB_sym = np.abs(outputB_fft)*np.cos(w*t+np.angle(outputB_fft))
-outputC_sym = np.abs(outputC_fft)*np.cos(w*t+np.angle(outputC_fft))
+t = current_deltaC[:,0]
 
 
 # output_modal = output*T_dq0
@@ -101,57 +76,122 @@ plt.ylabel('Current (kA)')
 plt.show()
 
 # AGG 2
-print(time_vector)
+# print(time_vector)
 # contador = np.size(current_deltaA)
-contador = 580
+contador = 600
 Ad_k_vector = np.zeros((contador))
 Aq_k_vector = np.zeros((contador))
 A0_k_vector = np.zeros((contador))
-print('Loop')
+
+Vd_k_vector = np.zeros((contador))
+Vq_k_vector = np.zeros((contador))
+V0_k_vector = np.zeros((contador))
+# print('Loop')
 for i in range(contador):
-  print('Iteración')
+#   print('Iteración')
   # Vector ABC
-  print('Impresion de Aabc_k')
+#   print('Impresion de Aabc_k')
   Aabc_k = np.array([[[current_deltaA[i,1]], [current_deltaB[i,1]], [current_deltaC[i,1]]]])
-  print(Aabc_k)
+  Vabc_k = np.array([[[voltage_deltaA[i,1]], [voltage_deltaB[i,1]], [voltage_deltaC[i,1]]]])
+#   print(Aabc_k)
 
   # Construcción Matriz Tdq0
-  print('Impresion de phi')
+#   print('Impresion de phi')
   phi = i*delta_t*2*np.pi*60 + theta
-  print(phi)
-  print('Impresion de Matriz')
+#   print(phi)
+#   print('Impresion de Matriz')
   T_dq0 = (2/3) * np.array([[0.5, 0.5, 0.5], [np.cos(phi),np.cos(phi-np.pi), np.cos(phi+np.pi) ], [-np.sin(phi),-np.sin(phi-np.pi), -np.sin(phi+np.pi)]])
-  print(T_dq0)
+#   print(T_dq0)
 
   # Cálculo de la trasnformada
-  print('Impresion de Park')
+#   print('Impresion de Park')
   Adq0_k = np.dot(T_dq0, Aabc_k)
-  print(Adq0_k)
+  Vdq0_k = np.dot(T_dq0, Vabc_k)
+#   print(Adq0_k)
 
   # Almacenamiento de valores en vectores
-  print('Impresion de Almacenamiento vector')
+#   print('Impresion de Almacenamiento vector')
   Ad_k = Adq0_k[0]
   Aq_k = Adq0_k[1]
   A0_k = Adq0_k[2]
-  print(Ad_k)
+
+  Vd_k = Vdq0_k[0]
+  Vq_k = Vdq0_k[1]
+  V0_k = Vdq0_k[2]
+#   print(Ad_k)
 
   Ad_k_vector[i] = Ad_k
   Aq_k_vector[i] = Aq_k
   A0_k_vector[i] = A0_k
-  print(Ad_k_vector)
+
+  Vd_k_vector[i] = Vd_k
+  Vq_k_vector[i] = Vq_k
+  V0_k_vector[i] = V0_k
+#   print(Ad_k_vector)
   if i == (contador - 1):
         print("The end")
         break
-  print('Número de iteración')    
-  print(i)
-  print('Impresion contador')
-  print(contador)
+#   print('Número de iteración')    
+#   print(i)
+#   print('Impresion contador')
+#   print(contador)
 
-print('Impresion componente directa')
-print(Ad_k_vector)
-print('Impresion componente cuadratura')
-print(Aq_k_vector)
-print('Impresion componente eje 0')
-print(A0_k_vector)
+# print('Impresion componente directa')
+# print(Ad_k_vector)
+# print('Impresion componente cuadratura')
+# print(Aq_k_vector)
+# print('Impresion componente eje 0')
+# print(A0_k_vector)
+
+plt.plot(t, Ad_k_vector)
+plt.plot(t, Aq_k_vector)
+plt.plot(t, A0_k_vector)
+# plt.plot(t,outputB[:,1])
+plt.xlabel('Time (s)')
+plt.ylabel('Current (kA)')
+plt.show()
+
+S_forward = Vd_k_vector + ZC*Ad_k_vector
+S_backward = Vd_k_vector - ZC*Ad_k_vector
+
+#FILTER
+  # Cut-off frequency of the filter
+
+tau = 1560000
+tau_arr = np.array(tau)
+w = 20e03/tau
+# Normalize the frequency
+b1, a1 = signal.butter(3, w, 'low')
+S_f = signal.filtfilt(b1, a1, S_forward,axis=0)
+
+b2, a2 = signal.butter(3, w, 'low')
+S_b = signal.filtfilt(b2, a2, S_backward,axis=0)
+
+
+plt.plot(t, S_f)
+plt.plot(t, S_b)
+# plt.plot(t,outputB[:,1])
+plt.xlabel('Time (s)')
+plt.ylabel('Current (kA)')
+plt.show()
+
+S_template = S_f[199:500]
+
+
+#     corr_fn = (1/(contador/2))*S_b[i*delta_t+tau_arr]*S_template[i*delta_t]
+#     corr_fns = corr_fns + corr_fn
+
+# print(corr_fns)
+
+corr = np.correlate(S_template,S_b)
+corr = corr*np.abs(corr)
+print(np.argmax(np.abs(corr),axis=0))
+tau_f = np.argmax(np.abs(corr),axis=0)
+
+d = ((3*10**8*tau_f*delta_t)/(2))*10e-03
+print(d)
+
+
+
 
 
