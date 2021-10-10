4e582ef4f51d7384cee9bcc40a0671d21974e15c
accruz <accruz@uninorte.edu.co>
2021-10-10 13:44:38 -0500

    Interfaz Gráfica Inicial Pt II

--- Assembly_FaultLocGUI.py
+++ Assembly_FaultLocGUI.py
@@ -1,31 +1,36 @@
+from zipfile import ZIP_MAX_COMMENT
 import numpy as np
 from numpy.core.fromnumeric import transpose
+from numpy.lib.npyio import zipfile_factory
 import pandas as pd 
 import matplotlib.pyplot as plt
 from scipy import signal
-
 import tkinter
-from tkinter import filedialog
+from tkinter import StringVar, filedialog
+from tkinter import ttk
 import csv
+global filename
+filename = ""
+global delta_t
+delta_t = 1
+global velprop
+velprop = 1
+global Zc
+Zc = 1+2j
+global faultdistance
+faultdistance = 20
+global combo
+
+
 win = tkinter.Tk()
 
 
-def UploadAction(delta_t):
-    global strfilename
+def UploadAction():
+    global filename
     filename = filedialog.askopenfilename()
-    FaultLoc(filename)
-    print(delta_t)
+    
 
-
-win.geometry("400x300")
-win.title("Aplicación para localización de fallas - PF202130")
-entry = tkinter.Entry(win)
-entry.place(x = 50, y = 50)
-delta_t = entry.get
-button = tkinter.Button(win, text='Open', command=UploadAction(delta_t))
-button.pack()
-
-def FaultLoc(filename):
+def FaultLoc():
     data = pd.read_csv(filename, delimiter=",") 
 
     print(data)
@@ -49,21 +54,67 @@ def FaultLoc(filename):
 
     print(voltage_deltaA)
     
-    # # ADC
-    # k = 1/ (1.56e06/2)
-    # theta = 0
+    # ADC
+    k = 1/(1.56e06/2)
+    theta = 0
 
+    # IMPEDANCIA CARACTERISTICA
+    Z = (20.631 + 89.93j)/110
+    Y = (676.75e-06j)/110
+
+    # ZC = Zc
+    ZC = np.sqrt(Z/Y)
     
-    # ZC = np.sqrt(Z/Y)
+    #  TRANSFORMACION MODAL
 
-    # # print(ZC)
-
-    # #  TRANSFORMACION MODAL
-
-    # w = 2*np.pi*60
-    # t = time_vector
-
+    w = 2*np.pi*60
+    t = time_vector
     
+    plt.plot(t, current_deltaB)
+    plt.plot(t, current_deltaA)
+    plt.plot(t, current_deltaC)
+
+win.geometry("400x300")
+win.title("Aplicación para localización de fallas - PF202130")
+
+entry = tkinter.Entry(win)
+entry.place(x = 50, y = 50)
+entry2 = tkinter.Entry(win)
+entry2.place(x = 50, y = 80)
+entry3 = tkinter.Entry(win)
+entry3.place(x = 50, y = 110)
+entry4 = tkinter.Entry(win)
+entry4.place(x = 50, y = 140)
+
+def VarReading():
+    global delta_t
+    global velprop
+    global Zc
+    global faultdistance
+    
+    delta_t = entry.get()
+    velprop = entry2.get()
+    Zc = entry3.get()
+    faultdistance = entry4.get()
+    
+button = tkinter.Button(win, text='Open', command=UploadAction)
+button.pack()
+buttonrun = tkinter.Button(win, text='Execute Program', command=lambda:[VarReading(), FaultLoc()])
+buttonrun.pack()
+
+
+widget_var = tkinter.StringVar()
+combo = ttk.Combobox(win,textvariable=widget_var,values=["Señales de entrada", "Señales transformadas", "Ondas Viajeras", "Correlación"], state="readonly")
+combo.set("Seleccione la gráfica...")
+combo.place(x = 100, y = 140)
+
+def Graph():
+    value = combo.get()
+    print(value)
+
+
+buttongraph = tkinter.Button(win, text='Graficar', command=Graph)
+buttongraph.pack()
 
 win.mainloop()
 
