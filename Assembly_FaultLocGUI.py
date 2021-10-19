449c312cf2237de2c7d782d1cf0710ebb71065cc
accruz <accruz@uninorte.edu.co>
2021-10-19 18:06:02 -0500

    Interfaz Gráfica Ult

--- Assembly_FaultLocGUI.py
+++ Assembly_FaultLocGUI.py
@@ -6,8 +6,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import signal
 import tkinter
-from tkinter import StringVar, filedialog
-from tkinter import ttk
+from tkinter import StringVar, filedialog, Canvas, ttk
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+
 import csv
 global filename
 filename = ""
@@ -16,7 +21,7 @@ delta_t = 1
 global velprop
 velprop = 1
 global Zc
-Zc = 1+2j
+Zc = 1
 global faultdistance
 faultdistance = 20
 global combo
@@ -74,17 +79,25 @@ def FaultLoc():
     plt.plot(t, current_deltaA)
     plt.plot(t, current_deltaC)
 
-win.geometry("400x300")
+win.geometry("1280x720")
 win.title("Aplicación para localización de fallas - PF202130")
 
+
+label1 = tkinter.Label(win,text = "Ingrese el valor de la frecuencia de muestreo:").place(x = 20, y = 50)  
+label2 = tkinter.Label(win,text = "Ingrese el valor de la velocidad de propagación:").place(x = 20, y = 80) 
+label3 = tkinter.Label(win,text = "Ingrese el valor de la impedancia característica:").place(x = 20, y = 110) 
+label4 = tkinter.Label(win,text = "Ingrese la distancia de falla:").place(x = 20, y = 140) 
+label5 = tkinter.Label(win,text = "Programa para la localización de fallas utilizando ondas viajeras:", font=('Helvetica', 18, 'bold')).place(x = 360, y = 10)  
+
+
 entry = tkinter.Entry(win)
-entry.place(x = 50, y = 50)
+entry.place(x = 280, y = 50)
 entry2 = tkinter.Entry(win)
-entry2.place(x = 50, y = 80)
+entry2.place(x = 280, y = 80)
 entry3 = tkinter.Entry(win)
-entry3.place(x = 50, y = 110)
+entry3.place(x = 280, y = 110)
 entry4 = tkinter.Entry(win)
-entry4.place(x = 50, y = 140)
+entry4.place(x = 280, y = 140)
 
 def VarReading():
     global delta_t
@@ -97,24 +110,53 @@ def VarReading():
     Zc = entry3.get()
     faultdistance = entry4.get()
     
-button = tkinter.Button(win, text='Open', command=UploadAction)
-button.pack()
-buttonrun = tkinter.Button(win, text='Execute Program', command=lambda:[VarReading(), FaultLoc()])
-buttonrun.pack()
+button = tkinter.Button(win, text='Abrir archivo .csv', command=UploadAction)
+button.place(x = 1120, y = 50)
+buttonrun = tkinter.Button(win, text='Ejecutar Script', command=lambda:[VarReading(), FaultLoc()])
+buttonrun.place(x = 1130, y = 80)
 
 
 widget_var = tkinter.StringVar()
 combo = ttk.Combobox(win,textvariable=widget_var,values=["Señales de entrada", "Señales transformadas", "Ondas Viajeras", "Correlación"], state="readonly")
 combo.set("Seleccione la gráfica...")
-combo.place(x = 100, y = 140)
+combo.place(x = 1100, y = 140)
 
 def Graph():
     value = combo.get()
-    print(value)
+    if value == "Señales de entrada":
+        print(value)
+        fig = Figure(figsize=(6, 6), dpi=100)
+        t = np.arange(0, 3, .01)
+        plot1 = fig.add_subplot(111)
+        plot1.plot(t, 2 * np.sin(2 * np.pi * t), 'r', t,2 * np.cos(2 * np.pi * t), 'b')
+        canvas1 = FigureCanvasTkAgg(fig, master=win)  # A tk.DrawingArea.
+        canvas1.draw()
+        canvas1.get_tk_widget().pack(padx=350, pady=180)
+        toolbar = NavigationToolbar2Tk(canvas1, win)
+        toolbar.update()
+        canvas1.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+    elif value == "Señales transformadas":
+        print(value)
+        fig = Figure(figsize=(6, 6), dpi=100)
+        t = np.arange(0, 3, .01)
+        plot1 = fig.add_subplot(111)
+        plot1.plot(t, 2 * np.tan(2 * np.pi * t))
+        canvas1 = FigureCanvasTkAgg(fig, master=win)  # A tk.DrawingArea.
+        canvas1.draw()
+        canvas1.get_tk_widget().pack(padx=350, pady=150)
+        toolbar = NavigationToolbar2Tk(canvas1, win)
+        toolbar.update()
+        canvas1.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+    elif value == "Ondas Viajeras":
+        print(value)
+    elif value == "Correlación":
+        print(value)
 
 
-buttongraph = tkinter.Button(win, text='Graficar', command=Graph)
-buttongraph.pack()
+buttongraph = tkinter.Button(win, text='Realizar Gráfica', command=Graph)
+buttongraph.place(x = 1125, y = 110)
 
 win.mainloop()
 
--- FaultLocGUI.py
+++ FaultLocGUI.py
@@ -1,19 +1,24 @@
-import tkinter as tk
-from tkinter.filedialog import askopenfilename
-import pandas as pd
+from tkinter import *
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+root = Tk()
+root.geometry('600x500+10+10')
 
+def do_plot(x, y):
+    [ax[x].clear() for x in range(4)]
+    ax[1].plot(x,y)
+    canvas.draw()
 
-def import_csv_data():
-    global v
-    csv_file_path = askopenfilename()
-    print(csv_file_path)
-    v.set(csv_file_path)
+frame1 = Frame(root); frame1.place(x=0, y=0, width=400, height=400)
+figure = plt.Figure(figsize=(5,5), facecolor='yellow')
+canvas = FigureCanvasTkAgg(figure, frame1)
+canvas.get_tk_widget().place(x=0,y=0,width=500,height=500)
+ax = [figure.add_subplot(2, 2, x+1) for x in range(4)]
 
-root = tk.Tk()
-tk.Label(root, text='File Path').grid(row=0, column=0)
-v = tk.StringVar()
-entry = tk.Entry(root, textvariable=v).grid(row=0, column=1)
-tk.Button(root, text='Browse Data Set',command=import_csv_data).grid(row=1, column=0)
-tk.Button(root, text='Close',command=root.destroy).grid(row=1, column=1)
+frame2 = Frame(root); frame2.place(x=500, y=0, width=100, height=400)
+btplot1 = Button(frame2, text='plot 1', command= lambda: do_plot([0,1,2],[5,3,7]))
+btplot1.place(x=0, y=50, width=50, height=20)
+btplot2 = Button(frame2, text='plot 2', command= lambda: do_plot([5,6,7],[3,8,2]))
+btplot2.place(x=0, y=100, width=50, height=20)
+
 root.mainloop()
-
